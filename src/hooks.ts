33a7c282237f454e7b5b453c3e7399f4471357b1
PaulFWatts <paul.watts@me.com>
2021-12-27 20:30:15 +0000

    Add DELETE endpoint

---
+++ src/hooks.ts
@@ -0,0 +1,10 @@
+import type { Handle } from "@sveltejs/kit";
+
+export const handle: Handle = async ({request, resolve}) => {
+    if (request.query.has("_method")) {
+        request.method = request.query.get("_method").toUpperCase();
+    }
+
+    const responce = await resolve(request);
+    return responce;
+};
